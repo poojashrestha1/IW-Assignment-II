ad129ba6cdbd64f1755e35c748900e108a640493
pooja-shres <poojashres7@gmail.com>
2020-07-05 15:46:50 +0545

    [updated] complete

---
+++ 16.py
@@ -0,0 +1,23 @@
+'''
+Imagine you are creating a Super Mario game. You need to define
+a class to represent Mario. What would it look like? If you aren't
+familiar with SuperMario, use your own favorite video or board game
+to model a player.
+'''
+
+class Player:
+    x = 10
+    y = 10
+    speed = 1
+ 
+    def moveRight(self):
+        self.x = self.x + self.speed
+ 
+    def moveLeft(self):
+        self.x = self.x - self.speed
+ 
+    def moveUp(self):
+        self.y = self.y - self.speed
+ 
+    def moveDown(self):
+        self.y = self.y + self.speed
